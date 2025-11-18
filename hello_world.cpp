836860f334eca7ac83528f51a7b2d2adfb52108d
Max <kefirka552@gmail.com>
2025-11-18 10:47:53 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 
-// Главная фунция - точка входа в программу
+// Главная фунция - точка входа в программу boom
 int main(){
 
-    std::string name; // Переменная имени пользователя
+    std::string name; // Переменная имени пользователя booom
     std::cout << "Enter your name: ";
-    std::cin >> name; // Чтение ввода пользователя
+    std::cin >> name; // Чтение ввода пользователя booom
 
-    std::cout << "Hello_world from " << name << std::endl; // Вывод приветственного сообщения
+    std::cout << "Hello_world from " << name << std::endl; // Вывод приветственного сообщения booom
 }
